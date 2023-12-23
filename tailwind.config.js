6af408ffdd8df35ca9cbf30ddcabda878991b8ec
shahulcb <theshahulofcl@gmail.com>
2023-12-24 01:02:46 +0530

    new colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBorderColor: "#292929",
+        customTextColor: "#808080",
+        customHoverColor: "#191919"
+      }
+    },
   },
   plugins: [],
 }
