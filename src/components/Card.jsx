e6cb0d40d48543bf1e6bc21a5ab49fa823938b89
shahulcb <theshahulofcl@gmail.com>
2023-12-28 00:25:07 +0530

    imageSrc props added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Card({ cardType, tag, title }) {
+function Card({ cardType, tag, title, imageSrc }) {
     return (
         <div className={`w-full h-full rounded-md flex ${cardType === "type1" ? "flex-col" : "flex-col lg:flex-row"} gap-4 cursor-pointer group`}>
             <div className={`${cardType === "type1" ? "w-full h-80" : "h-80 w-full lg:h-40 lg:w-60"} rounded-md overflow-hidden`}>
-                <img src="https://framerusercontent.com/images/isuy4zvThkzCNgjg9FrXesN4DJA.webp" alt="" className='w-full h-full object-cover rounded-md group-hover:scale-110 transition-all ease-in-out duration-500' />
+                <img src={imageSrc} alt="" className='w-full h-full object-cover rounded-md group-hover:scale-110 transition-all ease-in-out duration-500' />
             </div>
             <div className='flex flex-col gap-1'>
                 <p className='text-customTextColor text-xs font-medium'>{tag}</p>
