2564f33710cc46dbb424b63f51c5504dbf8690a3
shahulcb <theshahulofcl@gmail.com>
2023-12-28 02:16:09 +0530

    className added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button({ children }) {
+function Button({ children, className }) {
     return (
-        <div className='border border-customBorderColor w-full h-[50px] rounded-md flex justify-center items-center overflow-hidden cursor-pointer hover:bg-customHoverColor transition ease-in-out duration-500 group'>
+        <div className={`border border-customBorderColor w-full h-[50px] rounded-md flex justify-center items-center overflow-hidden cursor-pointer hover:bg-customHoverColor transition ease-in-out duration-500 group ${className}`}>
             <div className='relative text-nowrap transition ease-in-out duration-500 group-hover:-translate-y-[50px]'>
                 <span className=''>{children}</span>
                 <span className='absolute top-[50px] left-0'>{children}</span>
