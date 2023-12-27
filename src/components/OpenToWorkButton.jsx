353cca7183f69c33d8fa69312468e2bc3e357656
shahulcb <theshahulofcl@gmail.com>
2023-12-27 13:15:56 +0530

    open to work button

---
+++ src/components/OpenToWorkButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenToWorkButton() {
+    return (
+        <div className='px-4 py-2 bg-[#12fd7f0a] text-[#03e26b] flex justify-center items-center gap-4 rounded-md cursor-pointer hover:bg-[#12fd8011] transition ease-in-out duration-500'>
+            <span class="relative flex h-[10px] w-[10px] items-center justify-center">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#03e26b] opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-2 w-2  bg-[#03e26b]"></span>
+            </span>
+            <span className='text-base font-medium'>Open to work</span>
+        </div>
+    )
+}
+
+export default OpenToWorkButton
