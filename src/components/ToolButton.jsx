3077446af3ec20fb25f56be990e76cb67c61a9d1
shahulcb <theshahulofcl@gmail.com>
2023-12-28 00:45:33 +0530

    h-max

--- src/components/ToolButton.jsx
+++ src/components/ToolButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ToolButton({ imageSrc, title, tag }) {
     return (
-        <div className='border border-customBorderColor rounded-md p-3 flex items-center gap-4 cursor-pointer hover:bg-customHoverColor transition ease-in-out duration-500'>
+        <div className='border border-customBorderColor rounded-md p-3 h-max flex items-center gap-4 cursor-pointer hover:bg-customHoverColor transition ease-in-out duration-500'>
             <div className='w-9 h-9 rounded-sm'>
                 <img src={imageSrc} alt="" className='rounded-sm' />
             </div>
