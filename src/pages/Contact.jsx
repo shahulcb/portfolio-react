bc35deb291410dee06d32cdacdf5b92acbc7133a
shahulcb <theshahulofcl@gmail.com>
2023-12-29 15:29:32 +0530

    updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,12 +12,11 @@ function Contact() {
                 <h1 className="text-[32px] font-normal lg:text-[40px]">Let's Chat</h1>
                 <p className='text-base text-customTextColor font-light'>If you'd like to talk about a potential project or just say hi, send me a message or email me at <a href="" className='text-white hover:text-yellow-500 transition ease-in-out duration-500'>theshahulofcl@gmail.com</a></p>
                 <form className='flex flex-col gap-3'>
-                    <div className='flex gap-3'>
-                        <Input type={"text"} placeholder={"First name"} />
-                        <Input type={"text"} placeholder={"Last name"} />
+                    <div className='flex gap-3 flex-col md:flex-row'>
+                        <Input type={"text"} placeholder={"Name"} />
+                        <Input type={"email"} placeholder={"Email"} />
                     </div>
-                    <Input type={"email"} placeholder={"Email"} />
-                    <TextArea placeholder={"Leave a comment here"} rows={5} />
+                    <TextArea placeholder={"Leave a comment here"} rows={6} />
                     <SubmitButton>Send Message</SubmitButton>
                 </form>
             </Section>
