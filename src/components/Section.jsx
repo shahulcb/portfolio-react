9f95d2c54b93b277255c2be1a20aa128761ca4eb
shahulcb <theshahulofcl@gmail.com>
2024-01-01 13:14:19 +0530

    react-awesome-reveal added

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Fade } from "react-awesome-reveal"
 
 function Section({ children, className }) {
     return (
-        <section className={`p-6 lg:p-12 flex flex-col gap-[50px] border-b border-customBorderColor ${className}`} >
-            {children}
-        </section>
+        <Fade duration={1000} delay={200}>
+            <section className={`p-6 lg:p-12 flex flex-col gap-[50px] border-b border-customBorderColor bg-[#121212] ${className}`} >
+                {children}
+            </section>
+        </Fade>
     )
 }
 
