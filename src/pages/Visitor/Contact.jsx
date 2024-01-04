8cbfa31d251b28602f8b82ddfab8ec9aafcd615f
shahulcb <theshahulofcl@gmail.com>
2024-01-04 12:18:15 +0530

    formik and yup added

--- src/pages/Visitor/Contact.jsx
+++ src/pages/Visitor/Contact.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import Layout from "../Layout"
 import Section from "../../components/Section"
-import Input from "../../components/Form/Input"
-import TextArea from "../../components/Form/TextArea"
 import SubmitButton from "../../components/Form/SubmitButton"
+import { Formik, Form, ErrorMessage, Field } from 'formik'
+import * as Yup from 'yup';
 
 function Contact() {
+    const initialValues = {
+        name: '',
+        email: '',
+        comment: ''
+    }
+    const validationSchema = Yup.object({
+        name: Yup.string().required('Please enter your name'),
+        email: Yup.string().required('Please enter your email').email('Email format is invaid'),
+        comment: Yup.string().required('Please enter a comments')
+    })
+    const onSubmit = values => {
+        console.log(values);
+    }
     return (
         <Layout>
             <Section className={"border-none max-w-[700px] mx-auto lg:px-0"}>
                 <h1 className="text-[32px] font-normal lg:text-[40px]">Let's Chat</h1>
                 <p className='text-base text-customTextColor font-light'>If you'd like to talk about a potential project or just say hi, send me a message or email me at <a href="" className='text-white hover:text-yellow-500 transition ease-in-out duration-500'>theshahulofcl@gmail.com</a></p>
-                <form className='flex flex-col gap-3'>
-                    <div className='flex gap-3 flex-col md:flex-row'>
-                        <Input type={"text"} placeholder={"Name"} />
-                        <Input type={"email"} placeholder={"Email"} />
-                    </div>
-                    <TextArea placeholder={"Leave a comment here"} rows={6} />
-                    <SubmitButton>Send Message</SubmitButton>
-                </form>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                >
+                    <Form>
+                        <div className='flex flex-col gap-3'>
+                            <div className='flex gap-3 flex-col md:flex-row'>
+                                <div className='form-control'>
+                                    <label htmlFor='name'>Name</label>
+                                    <Field type="text" placeholder="name" name="name" id="name" />
+                                    <ErrorMessage name='name' component="span" />
+                                </div>
+                                <div className='form-control'>
+                                    <label htmlFor='email'>Email</label>
+                                    <Field type="email" placeholder="Eamil" name="email" id="email" />
+                                    <ErrorMessage name='email' component="span" />
+                                </div>
+                            </div>
+                            <div className='form-control'>
+                                <label htmlFor='comment'>Leave a comment here</label>
+                                <Field as="textarea" rows={5} placeholder="Leave a comment here" name="comment" id="comment" />
+                                <ErrorMessage name='comment' component="span" />
+                            </div>
+                            <SubmitButton>Send</SubmitButton>
+                        </div>
+                    </Form>
+                </Formik>
             </Section>
         </Layout>
     )
